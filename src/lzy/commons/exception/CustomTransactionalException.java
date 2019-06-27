0b9db1fd7a4335d40633f0e0ab0cbe215da9caab
lzy <lzy@l-zy>
2019-06-27 15:59:25 +0800

    新增自定义异常

---
+++ src/lzy/commons/exception/CustomTransactionalException.java
@@ -0,0 +1,64 @@
+/**********************************************************************************
+ * @Desc 
+ * @author lzy
+ * @date 2018年12月4日 下午6:04:05
+ **********************************************************************************/
+package lzy.commons.exception;
+
+/**********************************************************************************
+ * @Desc 
+ * @author lzy
+ * @date 2018年12月4日 下午6:04:05
+ **********************************************************************************/
+public class CustomTransactionalException extends RuntimeException {
+
+	/**********************************************************************************
+	 * @Desc 
+	 * @author lzy
+	 * @date 2018年12月4日 下午6:04:10
+	 **********************************************************************************/
+	private static final long serialVersionUID = 1L;
+	
+	private String            errCode;
+    private String            errMsg;
+
+    public CustomTransactionalException() {
+        super();
+    }
+
+
+    public CustomTransactionalException(String errMsg) {
+        super(errMsg);
+    }
+
+    public CustomTransactionalException(String errCode, String errMsg) {
+        super(errCode + ":" + errMsg);
+        this.errCode = errCode;
+        this.errMsg = errMsg;
+    }
+
+    public CustomTransactionalException(Throwable cause) {
+        super(cause);
+    }
+
+    public CustomTransactionalException(String errMsg, Throwable cause) {
+        super(errMsg, cause);
+    }
+
+    public CustomTransactionalException(String errCode, String errMsg , Throwable cause) {
+    	super(errCode + ":" + errMsg, cause);
+        this.errCode = errCode;
+        this.errMsg = errMsg;
+    }
+
+    public String getErrCode() {
+        return this.errCode;
+    }
+
+    public String getErrMsg() {
+        return this.errMsg;
+    }
+	
+	
+
+}
