560f31dad887725d6f2ac45aee023776c3aaa033
lzy <lzy@l-zy>
2019-07-24 15:26:30 +0800

    添加自定义注解

---
+++ src/lzy/commons/annotation/CustomLog.java
@@ -0,0 +1,21 @@
+package lzy.commons.annotation;
+
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/*****************************************************************************************************************************************************
+ * 异常日志注解
+ * 
+ * @author lzy 2019年3月4日 下午1:49:01
+ *****************************************************************************************************************************************************/
+@Documented//生成文档
+@Retention(RUNTIME)//注解在哪个阶段执行
+@Target(METHOD)//注解放置的目标位置,METHOD是可注解在方法级别上
+public @interface CustomLog {
+	String value() default "";
+}
--- src/lzy/commons/utils/LogUtils.java
+++ src/lzy/commons/utils/LogUtils.java
@@ -13,8 +13,15 @@ import org.slf4j.LoggerFactory;
  *****************************************************************************************************************************************************/
 public class LogUtils {
 	
-	public static Logger getLog(Class<?> clazz) {
-		Logger log = LoggerFactory.getLogger(clazz);
+	public static Logger getLog(Object clazz) {
+		Logger log = null;
+		if(clazz instanceof Class<?>) {
+			log = LoggerFactory.getLogger((Class<?>)clazz);
+		}else if(clazz instanceof String) {
+			log = LoggerFactory.getLogger((String)clazz);
+		}else {
+			throw new IllegalArgumentException("非法参数，日志实例仅支持：Class<?>或者String类型");
+		}
 		return log;
 	}
 
