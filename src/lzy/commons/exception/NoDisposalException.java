528e5675e9ef884f83676ce5165af63e430e60af
lzy <lzy@l-zy>
2019-09-02 14:51:10 +0800

    第一次提交

--- src/lzy/commons/exception/NoDisposalException.java
+++ src/lzy/commons/exception/NoDisposalException.java
@@ -6,11 +6,11 @@
 package lzy.commons.exception;
 
 /**********************************************************************************
- * 
+ * 不需要处理的异常
  * @author lzy
 * 2018年12月4日 下午6:04:05
  **********************************************************************************/
-public class HandleException extends Throwable {
+public class NoDisposalException extends Throwable {
 	
 
 	/**********************************************************************************
@@ -23,30 +23,30 @@ public class HandleException extends Throwable {
 	private String            errCode="NO_CODE";
     private String            errMsg;
 
-    public HandleException() {
+    public NoDisposalException() {
         super();
     }
 
 
-    public HandleException(String errMsg) {
+    public NoDisposalException(String errMsg) {
         super(errMsg);
     }
 
-    public HandleException(String errCode, String errMsg) {
+    public NoDisposalException(String errCode, String errMsg) {
         super(errCode + ":" + errMsg);
         this.errCode = errCode;
         this.errMsg = errMsg;
     }
 
-    public HandleException(Throwable cause) {
+    public NoDisposalException(Throwable cause) {
         super(cause);
     }
 
-    public HandleException(String errMsg, Throwable cause) {
+    public NoDisposalException(String errMsg, Throwable cause) {
         super(errMsg, cause);
     }
 
-    public HandleException(String errCode, String errMsg , Throwable cause) {
+    public NoDisposalException(String errCode, String errMsg , Throwable cause) {
     	super(errCode + ":" + errMsg, cause);
         this.errCode = errCode;
         this.errMsg = errMsg;
---
+++ src/lzy/commons/utils/http/HttpUtils.java
@@ -0,0 +1,124 @@
+/*****************************************************************************************************************************************************
+ * 
+ * @author lzy 2019年8月1日 下午5:33:40
+ *****************************************************************************************************************************************************/
+package lzy.commons.utils.http;
+
+import java.io.IOException;
+import java.util.Map;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+/*****************************************************************************************************************************************************
+ * 
+ * @author lzy 2019年8月1日 下午5:33:40
+ *****************************************************************************************************************************************************/
+public class HttpUtils {
+
+	/*****************************************************************************************************************************************************
+	 * desc: get请求
+	 * @param url 请求地址
+	 * @param header 请求头 可以为空
+	 * @return 返回相应结果
+	 * @author lzy 2019年8月1日 下午6:10:53
+	 *****************************************************************************************************************************************************/
+	public static String get(String url,Map<String,String> header) {
+		String entityStr = null;
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		try {
+			// 创建httpget
+			HttpGet httpGet = new HttpGet(url);
+			// 添加头部信息
+			if(header!=null) {
+				header.forEach((key,val) -> {
+					httpGet.addHeader(key,val);
+				});
+			}
+			// 执行get请求
+			CloseableHttpResponse response = httpClient.execute(httpGet);
+			try {
+				// 获取响应实体     
+				HttpEntity entity = response.getEntity();
+				if(entity!=null) {
+					entityStr = EntityUtils.toString(entity);
+				}
+			} finally {
+				response.close();
+			}
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				httpClient.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return entityStr;
+	}
+	
+	
+
+	/*****************************************************************************************************************************************************
+	 * desc: get请求
+	 * @param url 请求地址
+	 * @param header 请求头 可以为空
+	 * @param entity http实体，主要是传递参数数据 可以为空
+	 * @return 返回相应结果
+	 * @author lzy 2019年8月1日 下午6:10:53
+	 * @param entity 
+	 *****************************************************************************************************************************************************/
+	public static String post(String url,Map<String,String> header, HttpEntity entity) {
+		String entityStr = null;
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		try {
+			// 创建httpget
+			HttpPost httpPost = new HttpPost(url);
+			if(entity!=null) {
+				httpPost.setEntity(entity);
+			}
+			// 添加头部信息
+			if(header!=null) {
+				header.forEach((key,val) -> {
+					httpPost.addHeader(key,val);
+				});
+			}
+			// 执行get请求
+			CloseableHttpResponse response = httpClient.execute(httpPost);
+			try {
+				// 获取响应实体     
+				HttpEntity responseEntity = response.getEntity();
+				if(responseEntity!=null) {
+					entityStr = EntityUtils.toString(responseEntity);
+				}
+			} finally {
+				response.close();
+			}
+		} catch (ClientProtocolException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				httpClient.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return entityStr;
+	}
+}
