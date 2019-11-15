59dcb9519a496ceb0cbde87b5ee23a9d725eda71
lzy <lzy@192.168.5.36>
2019-11-15 16:48:18 +0800

    修改分表分库注解

---
+++ src/lzy/commons/annotation/BranchDataBase.java
@@ -0,0 +1,41 @@
+package lzy.commons.annotation;
+
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.TYPE_PARAMETER;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Documented
+@Retention(RUNTIME)
+@Target({ TYPE, METHOD, TYPE_PARAMETER })
+/*****************************************************************************************************************************************************
+ * 使用方法，在mapper数据操作接口上添加DataSource注解，可以自己指定数据源切换判断字段，当前默认userId
+ * 
+ * @author lzy
+ * @dateTime 2019年4月9日 下午2:52:34
+ *****************************************************************************************************************************************************/
+public @interface BranchDataBase {
+	
+	/*****************************************************************************************************************************************************
+	 * 动态数据源名称，标识使用哪个动态数据源，默认数据源是data
+	 *切面方法中判断使用哪个动态数据源的依据
+	 * @return 动态数据源名称
+	 * @author lzy 2019年7月19日 下午6:00:23
+	 *****************************************************************************************************************************************************/
+	String dynamic() default "data";
+
+	/*****************************************************************************************************************************************************
+	 * 数据库分库条件字段名 
+	 * 值为：default 代表使用默认数据源
+	 * 值为：random 代表使用随机数据源
+	 * 值为：all 代表使用所有数据源---该方式当前没有找到实现方式
+	 * @return 动态数据源切换方式
+	 * @author lzy 2019年7月19日 下午6:02:05
+	 *****************************************************************************************************************************************************/
+	String field() default "userId"; //字段
+}
---
+++ src/lzy/commons/utils/num/OrderSerialNumber.java
@@ -0,0 +1,54 @@
+/*****************************************************************************************************************************************************
+ * 
+ * @author lzy 2019年11月14日 下午3:13:02
+ *****************************************************************************************************************************************************/
+package lzy.commons.utils.num;
+
+import java.io.File;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*****************************************************************************************************************************************************
+ * 
+ * @author lzy 2019年11月14日 下午3:13:02
+ *****************************************************************************************************************************************************/
+public class OrderSerialNumber extends SerialNumber {
+
+	protected final static SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+	protected DecimalFormat df = null;
+	public OrderSerialNumber(int width) {
+		if (width < 1) {
+			throw new IllegalArgumentException(
+					"Parameter length must be great than 1!");
+		}
+		char[] chs = new char[width];
+		for (int i = 0; i < width; i++) {
+			chs[i] = '0';
+		}
+		df = new DecimalFormat(new String(chs));
+	}
+
+	/*****************************************************************************************************************************************************
+	 * 方法未完成
+	 * (non-Javadoc)
+	 * 
+	 * @see lzy.commons.utils.num.SerialNumber#process()
+	 * @author lzy 2019年11月14日 下午3:13:02
+	 *****************************************************************************************************************************************************/
+	@Override
+	protected String process() {
+		Date date = new Date();
+		int n = 4;
+		return format(date) + format(n);
+	}
+
+	protected String format(Date date) {
+		return sdf.format(date);
+	}
+
+	protected String format(int num) {
+		return df.format(num);
+	}
+}
+	
